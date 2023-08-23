b71ac57a58817f09373b375b14ab5756dca8502d
Ethan Christensen <ethan.christensen@sdl.usu.edu>
2023-08-23 14:49:18 -0600

    Implement showing todos

--- src/bin/show_todos.rs
+++ src/bin/show_todos.rs
@@ -1,7 +1,17 @@
-use dbfun::models::*;
+use dbfun::{models::*, establish_connection};
 use diesel::prelude::*;
-
 fn main() {
   use dbfun::schema::todos::dsl::*;
-  
+  let connection = &mut establish_connection();
+  let results = todos
+    .filter(completed.eq(false))
+    .limit(10)
+    .select(Todo::as_select())
+    .load(connection)
+    .expect("Error loading todos");
+
+  println!("Displaying {} todos", results.len());
+  for todo in results {
+    println!("{}", todo.title);
+  }
 }
